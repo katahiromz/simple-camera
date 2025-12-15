3fece5bf728d468f98e4f9a6f31040ea5cb945dc
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-12-15 19:53:17 +0900

    emulateInsets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+// App.tsx
 import React, { useRef, useState, useEffect, useCallback } from 'react';
 import AdvancedCamera from './components/AdvancedCamera.tsx';
+import { emulateInsets } from './utils.ts';
 import './App.css';
 
 // 製品版か？
@@ -10,9 +12,10 @@ const BASE_URL = import.meta.env.BASE_URL;
 
 // アプリ
 function App() {
+  // 本番環境ではない場合、insetsをエミュレートする
+  if (!IS_PRODUCTION) { emulateInsets(); }
   return (
-    <AdvancedCamera>
-    </AdvancedCamera>
+    <AdvancedCamera></AdvancedCamera>
   );
 }
 
---
+++ src/utils.ts
@@ -0,0 +1,21 @@
+// insetsをエミュレートする
+export const emulateInsets = () => {
+  let custom = { top: 30, bottom: 30, left: 30, right: 30 };
+  // デバイスプリセット
+  const presets = {
+    'iPhone X-13': { top: 44, bottom: 34, left: 0, right: 0 },
+    'iPhone 14 Pro+': { top: 59, bottom: 34, left: 0, right: 0 },
+    'iPhone 15 Pro': { top: 59, bottom: 34, left: 0, right: 0 },
+    'Android': { top: 0, bottom: 24, left: 0, right: 0 },
+    'iPad Pro': { top: 24, bottom: 20, left: 0, right: 0 },
+    'なし': { top: 0, bottom: 0, left: 0, right: 0 },
+    'カスタム': custom
+  };
+  // CSS変数を更新
+  const values = presets['カスタム'];
+  //const values = presets['なし'];
+  document.documentElement.style.setProperty('--safe-inset-top', `${values.top}px`);
+  document.documentElement.style.setProperty('--safe-inset-bottom', `${values.bottom}px`);
+  document.documentElement.style.setProperty('--safe-inset-left', `${values.left}px`);
+  document.documentElement.style.setProperty('--safe-inset-right', `${values.right}px`);
+};
