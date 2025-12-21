6742b435b3452a1a91efcd95501f923dac7ee66e
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-12-21 21:50:23 +0900

    takePhoto on volume keys 2

--- src/components/AdvancedCamera.tsx
+++ src/components/AdvancedCamera.tsx
@@ -889,20 +889,18 @@ const AdvancedCamera: React.FC<AdvancedCameraProps> = ({
     };
   }, [status, draw]);
 
+  // 物理の音量ボタンを押されたら撮影
   useEffect(() => {
     // Android側から呼ばれるグローバル関数を定義
     (window as any).onPhysicalVolumeButton = () => {
-      // 撮影中やエラー時でなければ撮影を実行
-      if (!isRecording && !error) {
-        takePhoto();
-      }
+      takePhoto();
     };
 
     // コンポーネントがアンマウントされる時にクリーンアップ
     return () => {
       delete (window as any).onPhysicalVolumeButton;
     };
-  }, [takePhoto, isRecording, error]);
+  }, [takePhoto]);
 
   // --- ズーム・パンロジック ---
   const MOUSE_WHEEL_DELTA = 0.004;
